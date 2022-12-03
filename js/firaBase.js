a90fc089532f8dbb41b358451509577136512618
giorgi325 <giorgadzegiorgi2006@gmail.com>
2022-12-03 21:09:28 +0400

    Bphoto

--- js/firaBase.js
+++ js/firaBase.js
@@ -11,14 +11,6 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-function randomID() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    let r = (Math.random() * 16) | 0;
-    let v = c == "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
-
 function generateFirebaseItem(ID, value) {
   return {
     id: ID,
